cf5ff61edcbbdeba2819ae2b2d0dbc792d498e9b
RodrigoH95 <HRodrigo95@hotmail.com>
2023-04-26 19:59:46 -0300

    ahora se guarda el ancho de cada capa individualmente

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ class Layer {
     this.img = img;
     this.speed = speed;
     this.x = 0;
+    this.width = this.img.width;
   }
 
   speed() {
@@ -16,7 +17,6 @@ class Background {
     this.gameSpeed = gameSpeed;
     this.layers = [];
     this.x = 0;
-    this.layerWidth = null;
   }
 
   setSpeed(speed) {
@@ -30,19 +30,18 @@ class Background {
       const layer = new Layer(img, speed);
       this.layers.push(layer);
     }
-    this.layerWidth = this.layers[0].img.width; // Width de la primera capa
   }
 
   draw(ctx) {
     for (const layer of this.layers) {
-      ctx.drawImage(layer.img, layer.x, 0, this.layerWidth, this.screenHeight);
-      ctx.drawImage(layer.img, layer.x + this.layerWidth, 0, this.layerWidth, this.screenHeight);
+      ctx.drawImage(layer.img, layer.x, 0, layer.width, this.screenHeight);
+      ctx.drawImage(layer.img, layer.x + layer.width, 0, layer.width, this.screenHeight);
     }
   }
 
   update() {
     for (const layer of this.layers) {
       layer.x -= this.gameSpeed * layer.speed;
-      if (layer.x <= -this.layerWidth) layer.x = 0};
+      if (layer.x <= -layer.width) layer.x = 0};
     }
   }
